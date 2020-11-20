decd48f86404b52b06fdba53b49aa5d511a174de
Paula Rusti <rusti.paula78@gmail.com>
2020-11-20 19:35:43 +0200

    Added Login Scene

--- src/gui/login/LoginController.java
+++ src/gui/login/LoginController.java
@@ -1,4 +1,22 @@
 package gui.login;
 
+import gui.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import gui.sceneUtilities.SceneManager;
+
 public class LoginController {
+
+    @FXML
+    private Button registerButton;
+
+
+    public void loginButtonClicked(ActionEvent actionEvent) {
+//        Main.getI().changeSceneOnMainStage(SceneManager.Type.LOGIN);
+    }
+
+//    public void registerButtonClicked(ActionEvent actionEvent){ Main.getI().changeSceneOnMainStage(SceneManager.Type.REGISTER_USER);
+//
+//    }
 }
